3883aa26c8c7b1a63ea739a3f254b4433296399b
Proman4713 <proman4713@gmail.com>
2026-05-15 00:30:33 +0300

    Temporarily installed yq to use in chroot

--- patches/001_remove_consumer_snaps.bash
+++ patches/001_remove_consumer_snaps.bash
@@ -24,9 +24,12 @@ rm -rf /var/lib/snapd/desktop/applications/*firefox*
 rm -rf /var/lib/snapd/desktop/applications/*thunderbird*
 
 if [ -f /var/lib/snapd/seed/seed.yaml ]; then
-	yq 'del(.snaps[] | select(.name == "firefox"))' /var/lib/snapd/seed/seed.yaml
-	yq 'del(.snaps[] | select(.name == "thunderbird"))' /var/lib/snapd/seed/seed.yaml
+	wget -qO yq.sh https://github.com/mikefarah/yq/releases/download/v4.53.2/yq_linux_amd64
+	chmod +x ./yq.sh
+	./yq.sh 'del(.snaps[] | select(.name == "firefox"))' /var/lib/snapd/seed/seed.yaml
+	./yq.sh 'del.snaps[] | select(.name == "thunderbird"))' /var/lib/snapd/seed/seed.yaml
 	cat /var/lib/snapd/seed/seed.yaml
+	rm -f ./yq.sh
 fi
 
 # Leftovers
