02af7df9d3c87f9f5b21d34aa0323f5fb2734f15
Proman4713 <proman4713@gmail.com>
2026-07-06 17:24:16 +0300

    Improve systemd potential solution

--- patches/006_systemd.bash
+++ patches/006_systemd.bash
@@ -1,7 +1,8 @@
 # Install casper so that the initramfs is updated accordingly, will later uninstall through chroot after copying the
 #	INITRD and VMLINUZ files from the chroot. This could fix the problem with SystemD's
 #	`[FAILED] Failed to start initrd-switch-root.service - Switch Root.` error. Not that I did any research on it,
-#	but I found https://askubuntu.com/questions/1244623/make-a-custom-ubuntu-20-04-usb-boot-thumb-drive
+#	but I found 
+#	THIS BY NATURE REMOVES DRACUT BECAUSE IT DEPENDS ON INITRAMFS-TOOLS
 install_packages casper
 
 apt-get autoremove -y --purge
